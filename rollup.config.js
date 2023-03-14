68085636ee5845e90851ba1960b0e7e9a7199c3a
ren1244 <n1244506804@hotmail.com>
2023-03-14 10:21:53 +0800

    打包輸出 index.html

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
 import { terser } from "rollup-plugin-terser";
+import html  from '@rollup/plugin-html';
+import fs from 'fs';
+
 export default [
     {
         input: 'src/index.js',
@@ -8,6 +11,16 @@ export default [
             plugins: [
                 terser()
             ]
-        }
+        },
+        plugins: [
+            html({
+                template: function(param) {
+                    let html = fs.readFileSync('index.html', {encoding: 'utf-8'});
+                    let src = '<script src="src/index.js" type="module"></script>';
+                    let repl = '<script src="./index.bundle.js"></script>';
+                    return html.replace(src, repl);
+                }
+            }),
+        ]
     }
 ];
