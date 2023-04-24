130657004e3490eb547892bc4bbd5712e82f9034
ren1244 <n1244506804@hotmail.com>
2023-04-24 15:11:30 +0800

    Rename dist to docs

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ export default [
     {
         input: 'src/index.js',
         output: {
-            file: 'dist/index.bundle.js',
+            file: 'docs/index.bundle.js',
             format: 'iife',
             plugins: [
                 terser()
